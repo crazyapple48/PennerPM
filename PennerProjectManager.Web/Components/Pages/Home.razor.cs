d043b5fc2bf7d6655d5c141087b0209b40d623c8
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2026-05-03 20:25:17 -0700

    Make tasks list collapsable

--- PennerProjectManager.Web/Components/Pages/Home.razor.cs
+++ PennerProjectManager.Web/Components/Pages/Home.razor.cs
@@ -6,7 +6,10 @@ namespace PennerProjectManager.Web.Components.Pages;
 
 public partial class Home : ComponentBase
 {
+    private bool _isProjectSelected;
     private CategoryModel? _selectedCategory;
+    private ProjectModel? _selectedProject;
+
     [Inject] public required ICategoryClientService CategoryClientService { get; set; }
 
     [Parameter] public int? CategoryId { get; set; }
@@ -18,5 +21,22 @@ public partial class Home : ComponentBase
             _selectedCategory = await CategoryClientService.GetCategoryById(CategoryId.Value);
         else
             _selectedCategory = null;
+    }
+
+    private void OnProjectSelected(ProjectModel project)
+    {
+        if (_isProjectSelected)
+        {
+            _isProjectSelected = false;
+            Console.WriteLine("clicked " + project.Name + " is " + _isProjectSelected);
+            StateHasChanged();
+        }
+        else
+        {
+            _selectedProject = project;
+            _isProjectSelected = true;
+            Console.WriteLine("clicked " + project.Name + " is " + _isProjectSelected);
+            StateHasChanged();
+        }
     }
 }
