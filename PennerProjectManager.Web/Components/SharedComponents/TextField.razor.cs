1475ef14562f3de06a65745ee9e31f2a0e8cdff8
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2026-05-25 12:55:10 -0700

    Add re-useable textfield component.

---
+++ PennerProjectManager.Web/Components/SharedComponents/TextField.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+
+namespace PennerProjectManager.Web.Components.SharedComponents;
+
+public partial class TextField : ComponentBase
+{
+    [Parameter] public string? PlaceholderText { get; set; }
+    [Parameter] public string? Value { get; set; }
+    [Parameter] public EventCallback<string?> ValueChanged { get; set; }
+    [Parameter] public Expression<Func<string?>>? ValueExpression { get; set; }
+
+    public async void OnInput(ChangeEventArgs e)
+    {
+        await ValueChanged.InvokeAsync(e.Value?.ToString());
+    }
+}
